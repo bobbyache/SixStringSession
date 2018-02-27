de11f4f30ea53a4064ec9f797133cac5a3278059
Rob Blake <rob_bl8ke@hotmail.com>
2018-02-27 14:36:46 +0200

    Added first projects and first class "Goal" with initial unit tests.

---
+++ Code/CygSoft.SmartSession.GoalManagement.UnitTests/GoalTests.cs
@@ -0,0 +1,45 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.GoalManagement.UnitTests
+{
+    [TestFixture]
+    public class GoalTests
+    {
+        [Test]
+        public void Goal_New_Has_No_Minutes_Recorded()
+        {
+            Goal goal = new Goal();
+            Assert.AreEqual(0, goal.MinutesRecorded);
+        }
+
+        [Test]
+        public void Goal_Existing_Has_SetState_Via_Constructor()
+        {
+            int minutesRecorded = 12;
+            IGoalTask[] goalTasks = new IGoalTask[]
+            {
+                new Mock<IGoalTask>().Object,
+                new Mock<IGoalTask>().Object
+            };
+
+            IGoalFile[] goalFiles = new IGoalFile[]
+            {
+                new Mock<IGoalFile>().Object,
+                new Mock<IGoalFile>().Object
+            };
+            
+            Goal goal = new Goal(minutesRecorded, goalTasks, goalFiles);
+            Assert.AreEqual(12, goal.MinutesRecorded);
+            Assert.AreEqual(2, goal.FileCount);
+            Assert.AreEqual(2, goal.Files.Length);
+            Assert.AreEqual(2, goal.TaskCount);
+            Assert.AreEqual(2, goal.Tasks.Length);
+        }
+    }
+}
---
+++ Code/CygSoft.SmartSession.GoalManagement/Goal.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.GoalManagement
+{
+    public interface IGoalTask
+    {
+    }
+
+    public interface IGoalFile
+    {
+    }
+
+    internal class Goal
+    {
+        private IGoalFile[] goalFiles;
+        private IGoalTask[] goalTasks;
+
+        public Goal() { }
+
+        internal Goal(int minutesRecorded, IGoalTask[] goalTasks, IGoalFile[] goalFiles)
+        {
+            MinutesRecorded = minutesRecorded;
+            this.goalTasks = goalTasks;
+            this.goalFiles = goalFiles;
+        }
+
+        public double FileCount => goalFiles.Count();
+
+        public IGoalFile[] Files => goalFiles;
+        public int MinutesRecorded { get; internal set; }
+        public double TaskCount => goalTasks.Count();
+        public IGoalTask[] Tasks => goalTasks;
+    }
+}
