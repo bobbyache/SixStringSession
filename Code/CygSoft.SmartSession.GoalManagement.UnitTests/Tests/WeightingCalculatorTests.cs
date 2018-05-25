9e7a63c16a746d32b24b8ab356773b36bd3b5855
Rob Blake <robbie.a.blake@gmail.com>
2018-05-25 15:00:53 +0200

    Weightings working when adding and calculating weighting by dividing by number of items.

---
+++ Code/CygSoft.SmartSession.GoalManagement.UnitTests/Tests/WeightingCalculatorTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.GoalManagement.UnitTests.Tests
+{
+    [TestFixture]
+    public class WeightingCalculatorTests
+    {
+        [Test]
+        public void WeightingCalculator_Add_First_Item_Will_Be_100_Percent_Weighting()
+        {
+            WeightingCalculator calculator = new WeightingCalculator();
+            calculator.AddItem("001");
+            Assert.AreEqual(100, calculator["001"]);
+        }
+
+        [Test]
+        public void WeightingCalculator_Add_Second_Item_Will_Be_50_Percent_Weighting()
+        {
+            WeightingCalculator calculator = new WeightingCalculator();
+            calculator.AddItem("001");
+            calculator.AddItem("002");
+            Assert.AreEqual(50, calculator["001"]);
+            Assert.AreEqual(50, calculator["002"]);
+        }
+
+        [Test]
+        public void WeightingCalculator_Add_Third_Item_Will_Be_33_3_Percent_Weighting()
+        {
+            WeightingCalculator calculator = new WeightingCalculator();
+            calculator.AddItem("001");
+            calculator.AddItem("002");
+            calculator.AddItem("003");
+            //Assert.That(calculator["001"], Is.LessThan(33.4));
+            //Assert.That(calculator["001"], Is.LessThan(33.4));
+            Assert.That(calculator["001"], Is.InRange(33.3, 33.34));
+            Assert.That(calculator["002"], Is.InRange(33.3, 33.34));
+            Assert.That(calculator["003"], Is.InRange(33.3, 33.34));
+
+            //Assert.That(calculator["001"], Is.AtLeast(33.3)
+            //Assert.AreEqual(33.3, calculator["001"]);
+            //Assert.AreEqual(33.3, calculator["002"]);
+            //Assert.AreEqual(33.3, calculator["003"]);
+        }
+    }
+}
---
+++ Code/CygSoft.SmartSession.GoalManagement/WeightingCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.GoalManagement
+{
+    public class WeightingCalculator
+    {
+        private class Weighting
+        {
+            public string Id { get; set; }
+            public double Value { get; set; }
+        }
+
+        private Dictionary<string, Weighting> weightings = new Dictionary<string, Weighting>();
+
+        public bool Exists(string id) => weightings.ContainsKey(id);
+
+        public double this[string id] => weightings[id].Value;
+
+        public void AddItem(string id)
+        {
+            weightings.Add(id, new Weighting { Id = id, Value = 0 });
+            foreach (var item in weightings.Values)
+            {
+                item.Value = 100d / weightings.Count;
+            }
+        }
+
+        public void RemoveItem(string id)
+        {
+            weightings.Remove(id);
+        }
+
+        public void Change(string id, double value)
+        {
+            weightings[id].Value = value;
+        }
+    }
+}
