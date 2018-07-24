c3719a2d7340521256fdf2882e2c084eafa93236
Rob Blake <robbie.a.blake@gmail.com>
2018-07-24 12:25:37 +0200

    Added the timer app.

---
+++ Code/CygSoft.SmartSession.TimerApp/CygSoft.SmartSession.TimerApp/Countdown.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.TimerApp
+{
+    public class Countdown
+    {
+        public double CountdownSeconds { get; private set; }
+
+        public Countdown(double countdownSeconds)
+        {
+            CountdownSeconds = countdownSeconds;
+        }
+
+        public string GetValue(DateTime startTime, DateTime currentTime)
+        {
+            double totalSecondsElapsed = Math.Abs(new TimeSpan(startTime.Ticks).Subtract(new TimeSpan(currentTime.Ticks)).TotalSeconds);
+            double totalSecondsRemaining = CountdownSeconds - totalSecondsElapsed;
+
+            return DisplayTime(totalSecondsRemaining);
+        }
+
+        private string DisplayTime(double seconds)
+        {
+            TimeSpan t = TimeSpan.FromSeconds(seconds);
+            return t.ToString(@"hh\:mm\:ss");
+        }
+    }
+}
---
+++ Code/CygSoft.SmartSession.TimerApp/CygSoft.SmartSession.TimerApp/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace CygSoft.SmartSession.TimerApp
+{
+    public partial class Form1 : Form
+    {
+        private DateTime _startTime = DateTime.MinValue;
+        private System.Timers.Timer aTimer = new System.Timers.Timer();
+        private Countdown countDown = new Countdown(65);
+
+        public Form1()
+        {
+            InitializeComponent();
+            btnStartPause.Text = "Start";
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 500;
+        }
+
+        private void btnStartPause_Click(object sender, EventArgs e)
+        {
+            
+            if (_startTime == DateTime.MinValue)
+                _startTime = DateTime.Now;
+            aTimer.Enabled = !aTimer.Enabled;
+
+            btnStartPause.Text = btnStartPause.Text == "Start" ? "Pause" : "Start";
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            lblCurrentTaskTimeLeft.InvokeIfRequired(c => c.Text = countDown.GetValue(_startTime, DateTime.Now));
+
+            if (DateTime.Now >= _startTime.AddSeconds(countDown.CountdownSeconds))
+                Reset();
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            aTimer.Enabled = false;
+            _startTime = DateTime.MinValue;
+            btnStartPause.InvokeIfRequired(c => c.Text = "Start");
+        }
+    }
+}
