9cf91cf01a6d21e1152014182ccf8de63e4b855c
Rob Blake <robbie.a.blake@gmail.com>
2018-08-13 12:16:29 +0200

    Added initial database design.

---
+++ Design/Database/03 - Populate Lookup Tables.sql
@@ -0,0 +1,18 @@
+
+USE [SmartSession]
+GO
+
+MERGE 
+	GoalStatus AS target  
+USING 
+	(
+		SELECT			'CRE', 'Created'
+		UNION SELECT	'ACT', 'Active'
+	) 
+	AS source (Id, Status)  
+ON (target.Id = source.Id)  
+WHEN MATCHED THEN   
+        UPDATE SET Status = source.Status  
+WHEN NOT MATCHED THEN  
+    INSERT (Id, Status)  
+    VALUES (source.Id, source.Status);
---
+++ Design/Database/SPs/sp_InsertGoal.sql
@@ -0,0 +1,59 @@
+
+-- USE SmartSession
+--GO
+
+/* ===================================================================================================
+	Name:				sp_InsertGoal
+	CreatedBy:         	Rob Blake
+	CreateDate:        	13/08/2018
+	Modified by:       	Rob Blake
+
+	Insert a goal
+	
+Usage:
+------------------------------------------------------------------------------------------------------
+	EXEC SmartSession.dbo.sp_InsertGoal 'DevUser'
+=================================================================================================== */
+
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.ROUTINES WHERE ROUTINE_NAME = 'sp_InsertGoal')
+   DROP PROC sp_InsertGoal;
+GO
+
+CREATE PROCEDURE dbo.sp_InsertGoal
+(
+	@Title NVARCHAR(150),
+	@CreateDate DATETIME = NULL,
+	@Status NCHAR(3) = 'CRE', -- Created 
+	@TargetCompletionDate DATETIME = NULL
+
+)
+AS
+BEGIN
+	SET NOCOUNT ON;
+	
+	DECLARE @InsertDate DATETIME = GETDATE()
+	IF @CreateDate IS NULL SET @CreateDate = @InsertDate
+
+	INSERT INTO [dbo].[Goal]
+			   (
+			   [Title]
+			   ,[CreateDate]
+			   ,[TargetCompletionDate]
+			   ,[Status]
+			   )
+		VALUES
+		(
+			@Title,
+			@CreateDate,
+			@TargetCompletionDate,
+			@Status
+		)
+	
+END
+
+GO
