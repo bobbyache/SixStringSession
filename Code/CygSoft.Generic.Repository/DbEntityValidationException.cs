27ab784184cc05ce4081fbb6c507ac193c2c57c8
Rob Blake <robbie.a.blake@gmail.com>
2018-08-27 10:44:27 +0200

    Added repositories to the EF project.

---
+++ Code/CygSoft.Generic.Repository/DbEntityValidationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.Generic.Repository
+{
+    public class DbEntityValidationException : Exception
+    {
+        public DbEntityValidationException(string message, EntityValidationErrors[] entityValidationErrors) : base(message)
+        {
+            EntityValidationErrors = entityValidationErrors;
+        }
+
+        public EntityValidationErrors[] EntityValidationErrors { get; }
+    }
+
+    public class EntityValidationErrors
+    {
+        public IEnumerable<EntityValidationError> ValidationErrors { get; internal set; }
+    }
+
+    public class EntityValidationError
+    {
+        public string ErrorMessage { get; internal set; }
+    }
+}
---
+++ Code/CygSoft.Generic.Repository/IEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.Generic.Repository
+{
+    public interface IEntity
+    {
+        DateTime DateCreated { get; set; }
+        DateTime DateModified { get; set; }
+        string ModifiedBy { get; set; }
+        string CreatedBy { get; set; }
+    }
+}
