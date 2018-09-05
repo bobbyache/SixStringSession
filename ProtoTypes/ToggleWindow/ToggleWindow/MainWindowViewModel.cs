c6ea9109a68fe64c14d2bff7ba5c36b857f39cec
Rob Blake <robbie.a.blake@gmail.com>
2018-09-05 14:21:25 +0200

    Added the ToggleWindow prototype.

---
+++ ProtoTypes/ToggleWindow/ToggleWindow/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ToggleWindow
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private bool isItemsControlOpen;
+        public bool IsItemsControlOpen
+        {
+            get
+            {
+                return isItemsControlOpen;
+            }
+            set
+            {
+                isItemsControlOpen = value;
+                NotifyPropertyChanged("IsItemsControlOpen");
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
