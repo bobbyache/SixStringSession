17b5268c4509d8c9489721909e62f7839f169b8f
Rob Blake <robbie.a.blake@gmail.com>
2018-09-20 15:53:46 +0200

    Removed CygSoft.SmartSession.EF.UnitTests project. Added the beginnings of a UI validation strategy.

---
+++ Code/CygSoft.SmartSession.Desktop/Supports/Validators/RequiredTextFieldValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace CygSoft.SmartSession.Desktop.Supports.Validators
+{
+    public class RequiredTextFieldValidator : ValidationRule
+    {
+        public override ValidationResult Validate(object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            if (((string)value).Length < 4)
+                return new ValidationResult(false, "Field value must be supplied.");
+            //if (string.IsNullOrEmpty((string)value))
+            //    return new ValidationResult(false, "Field value must be supplied.");
+            else
+                return new ValidationResult(true, null);
+        }
+    }
+}
--- Code/CygSoft.SmartSession.EF.UnitTests/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CygSoft.SmartSession.EF.UnitTests")]
-[assembly: ComVisible(false)]
-
-[assembly: Guid("fd9a2e9a-b2a9-417f-aa45-0f7aa5df7c10")]
-
