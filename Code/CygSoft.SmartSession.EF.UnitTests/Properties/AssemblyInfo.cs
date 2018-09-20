c4ecf1547f639bf00d129b9a134ac63952a976c7
Rob Blake <robbie.a.blake@gmail.com>
2018-09-20 15:07:02 +0200

    Updated SharedAssemblyInfo and added it to some projects where it was missing.

--- Code/CygSoft.SmartSession.EF.UnitTests/Properties/AssemblyInfo.cs
+++ Code/CygSoft.SmartSession.EF.UnitTests/Properties/AssemblyInfo.cs
@@ -2,19 +2,8 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("UnitTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UnitTests")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
+[assembly: AssemblyTitle("CygSoft.SmartSession.EF.UnitTests")]
 [assembly: ComVisible(false)]
 
 [assembly: Guid("fd9a2e9a-b2a9-417f-aa45-0f7aa5df7c10")]
 
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
