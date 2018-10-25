ac05ae5e21e3adfd61b3e61c0696224091a09da9
Rob Blake <robbie.a.blake@gmail.com>
2018-10-25 10:39:07 +0200

    Noted that the connection string in the ContextFactory can be hard coded because its required only for database schema generation (on the test database).

--- Code/CygSoft.SmartSession.EF/SmartSessionContextFactory.cs
+++ Code/CygSoft.SmartSession.EF/SmartSessionContextFactory.cs
@@ -8,15 +8,19 @@ using System.Threading.Tasks;
 
 namespace CygSoft.SmartSession.EF
 {
+    /// <summary>
+    /// Source: https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/dbcontext-creation
+    /// 
+    /// This class is required for design time updates to the database schema. Therefore, it is ok to hard code the connection
+    /// string because the "update" will only go to the test database (as configured below). This connection string does not 
+    /// affect the connection for the application, which is stored in the GUI App.Config under connection strings.
+    /// 
+    /// To update the production database, do a "script-migration" and execute the updates related to specific release.
+    /// </summary>
     public class SmartSessionContextFactory : IDesignTimeDbContextFactory<SmartSessionContext>
     {
-        // https://docs.microsoft.com/en-us/ef/core/miscellaneous/cli/dbcontext-creation
-
         public SmartSessionContext CreateDbContext(string[] args)
         {
-            //var optionsBuilder = new DbContextOptionsBuilder<SmartSession>();
-            //optionsBuilder.UseSqlite("Data Source=blog.db");
-
             return new SmartSessionContext(@"server=ROBB-LT02\ROBLT;database=SmartSession_EF_Test;Integrated Security=True;Connection Reset=true;");
         }
     }
