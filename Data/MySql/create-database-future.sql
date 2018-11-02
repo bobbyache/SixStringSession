982180cb09b3274d48baef9a40dc67b8fbc45194
Rob Blake <robbie.a.blake@gmail.com>
2018-11-02 12:55:40 +0200

    Modified the current database to be as close as possible to the current EF database in order to get the repositories to start working before making schema changes.

--- Data/MySql/create-database-future.sql
+++ Data/MySql/create-database-future.sql
@@ -1,50 +1,41 @@
 
-/*
-	README !!!
-	Always want a copy of this, because your test database will often be ahead of your
-	production database.
-	Go to the Data\MySql folder to update/find the production or release scripts.
-*/
+DROP DATABASE IF EXISTS `smartsession`;
 
+CREATE DATABASE `smartsession` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 
-DROP DATABASE IF EXISTS `smartsession_tests`;
-
-CREATE DATABASE `smartsession_tests` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-
-USE `smartsession_tests`;
+USE `smartsession`;
 
 CREATE TABLE `exercise` (
-  `Id` int(11) NOT NULL,
+  `ID` int(11) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `DifficultyRating` int(11) NOT NULL,
-  `PracticalityRating` int(11)  NOT NULL,
+  `DifficultyRating` int(11) DEFAULT NULL,
+  `PracticalityRating` int(11) DEFAULT NULL,
   `PercentCompleteCalculationType` int(11) NOT NULL,
   `InitialMetronomeSpeed` int(11) DEFAULT NULL,
   `TargetMetronomeSpeed` int(11) DEFAULT NULL,
   `TargetPracticeTime` int(11) DEFAULT NULL,
   `DateCreated` datetime NOT NULL,
-  `DateModified` datetime NOT NULL,
+  `DateModified` datetime DEFAULT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `smartsession_tests`.`exerciseactivity` (
+CREATE TABLE `smartsession`.`exerciseactivity` (
   `Id` INT NOT NULL,
   `StartTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `EndTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `Seconds` INT NOT NULL,
-  `StartMetronomeSpeed` INT NOT NULL,
-  `AchievedMetronomeSpeed` INT NOT NULL,
-  `ComfortMetronomeSpeed` INT NOT NULL,
+  `StartSpeed` INT NULL,
+  `Speed` INT NOT NULL,
   `DateCreated` DATETIME NOT NULL COMMENT 'A generic exercise activity.',
-  `DateModified` DATETIME NOT NULL,
+  `DateModified` DATETIME NULL,
   PRIMARY KEY (`Id`));
 
-ALTER TABLE `smartsession_tests`.`exerciseactivity` 
+ALTER TABLE `smartsession`.`exerciseactivity` 
 ADD COLUMN `ExerciseId` INT NOT NULL AFTER `Id`;
-ALTER TABLE `smartsession_tests`.`exerciseactivity` 
+ALTER TABLE `smartsession`.`exerciseactivity` 
 ADD CONSTRAINT `fk_exercise`
   FOREIGN KEY (`Id`)
-  REFERENCES `smartsession_tests`.`exercise` (`Id`)
+  REFERENCES `smartsession`.`exercise` (`ID`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
--- Data/MySql/create-database.sql
+++ Data/MySql/create-database.sql
@@ -6,16 +6,16 @@ CREATE DATABASE `smartsession` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE ut
 USE `smartsession`;
 
 CREATE TABLE `exercise` (
-  `ID` int(11) NOT NULL,
+  `Id` int(11) NOT NULL,
   `Title` varchar(255) NOT NULL,
-  `DifficultyRating` int(11) DEFAULT NULL,
-  `PracticalityRating` int(11) DEFAULT NULL,
+  `DifficultyRating` int(11) NOT NULL,
+  `PracticalityRating` int(11)  NOT NULL,
   `PercentCompleteCalculationType` int(11) NOT NULL,
   `InitialMetronomeSpeed` int(11) DEFAULT NULL,
   `TargetMetronomeSpeed` int(11) DEFAULT NULL,
   `TargetPracticeTime` int(11) DEFAULT NULL,
   `DateCreated` datetime NOT NULL,
-  `DateModified` datetime DEFAULT NULL,
+  `DateModified` datetime NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -24,10 +24,11 @@ CREATE TABLE `smartsession`.`exerciseactivity` (
   `StartTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `EndTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `Seconds` INT NOT NULL,
-  `StartSpeed` INT NULL,
-  `Speed` INT NOT NULL,
+  `StartMetronomeSpeed` INT NOT NULL,
+  `AchievedMetronomeSpeed` INT NOT NULL,
+  `ComfortMetronomeSpeed` INT NOT NULL,
   `DateCreated` DATETIME NOT NULL COMMENT 'A generic exercise activity.',
-  `DateModified` DATETIME NULL,
+  `DateModified` DATETIME NOT NULL,
   PRIMARY KEY (`Id`));
 
 ALTER TABLE `smartsession`.`exerciseactivity` 
@@ -35,7 +36,7 @@ ADD COLUMN `ExerciseId` INT NOT NULL AFTER `Id`;
 ALTER TABLE `smartsession`.`exerciseactivity` 
 ADD CONSTRAINT `fk_exercise`
   FOREIGN KEY (`Id`)
-  REFERENCES `smartsession`.`exercise` (`ID`)
+  REFERENCES `smartsession`.`exercise` (`Id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
