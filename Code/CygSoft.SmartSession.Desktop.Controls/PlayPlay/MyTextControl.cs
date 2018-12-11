403bd69e9714361510873cf1d79f6daa8ebafb94
Rob Blake <robbie.a.blake@gmail.com>
2018-12-11 08:32:55 +0200

    Added the MyTextControl classes (can't remember why... but working on a time picker control suitable for the application).

---
+++ Code/CygSoft.SmartSession.Desktop.Controls/PlayPlay/MyTextControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CygSoft.SmartSession.Desktop.Controls
+{
+    public class MyTextControl : Control
+    {
+        static MyTextControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(
+                typeof(MyTextControl), new FrameworkPropertyMetadata(typeof(MyTextControl)
+                    ));
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }   
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(MyTextControl), 
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+
+    }
+}
