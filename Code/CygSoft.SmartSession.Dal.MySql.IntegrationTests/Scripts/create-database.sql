f6f18a5185b515e89d5830e22146e74689b08f9d
Rob Blake <robbie.a.blake@gmail.com>
2018-12-12 12:55:07 +0200

    Only create tables if it doesn't already exist.

--- Code/CygSoft.SmartSession.Dal.MySql.IntegrationTests/Scripts/create-database.sql
+++ Code/CygSoft.SmartSession.Dal.MySql.IntegrationTests/Scripts/create-database.sql
@@ -1,20 +1,18 @@
 
-/*
-	README !!!
-	Always want a copy of this, because your test database will often be ahead of your
-	production database.
-	Go to the Data\MySql folder to update/find the production or release scripts.
-*/
-
-
-
 DROP DATABASE IF EXISTS `smartsession_tests`;
-
-CREATE DATABASE `smartsession_tests` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
-
+CREATE DATABASE `smartsession_tests` /* !40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */;
 USE `smartsession_tests`;
 
-CREATE TABLE `exercise` (
+/* ********************************************************************************************************************************
+TEST DATABASE
+-----------------------------------------------------------------------------------------------------------------------------------
+	This database is the first database in the line of attack.
+	When you're happy with all tests on this database then you can run this script against the QA database (smartsession_qa). 
+
+	Go to the Data\MySql folder to update/find the production or release scripts.
+******************************************************************************************************************************** */
+
+CREATE TABLE IF NOT EXISTS `exercise` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `Title` varchar(255) NOT NULL,
   `DifficultyRating` int(11) NOT NULL,
@@ -25,25 +23,18 @@ CREATE TABLE `exercise` (
   `TargetPracticeTime` int(11) DEFAULT NULL,
   `DateCreated` datetime NOT NULL,
   `DateModified` datetime NULL,
-  PRIMARY KEY (`ID`)
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `smartsession_tests`.`exerciseactivity` (
+CREATE TABLE IF NOT EXISTS `smartsession_tests`.`exerciseactivity` (
   `Id` INT NOT NULL AUTO_INCREMENT,
+  `ExerciseId` INT NOT NULL,
   `StartTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `EndTime` DATETIME NOT NULL COMMENT 'Time is not calculated from StartTime to EndTime, these metrics are just there in order to give context and attach to a diary.',
   `Seconds` INT NOT NULL,
   `MetronomeSpeed` INT NOT NULL,
   `DateCreated` DATETIME NOT NULL COMMENT 'A generic exercise activity.',
   `DateModified` DATETIME NULL,
-  PRIMARY KEY (`Id`));
-
-ALTER TABLE `smartsession_tests`.`exerciseactivity` 
-ADD COLUMN `ExerciseId` INT NOT NULL AFTER `Id`;
-ALTER TABLE `smartsession_tests`.`exerciseactivity` 
-ADD CONSTRAINT `fk_exercise`
-  FOREIGN KEY (`ExerciseId`)
-  REFERENCES `smartsession_tests`.`exercise` (`Id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-
+  PRIMARY KEY (`Id`),
+  CONSTRAINT `fk_exerciseactivity_exercise` FOREIGN KEY (`ExerciseId`) REFERENCES `exercise` (`Id`)
+);
