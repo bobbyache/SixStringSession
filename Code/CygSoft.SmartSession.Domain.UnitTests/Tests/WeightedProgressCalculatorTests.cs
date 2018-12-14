461319d30d9e7078853165f9d2dc86ce283fd6b8
Rob Blake <robbie.a.blake@gmail.com>
2018-12-14 10:57:33 +0200

    WeightedProgressCalculator reintroduced from legacy code.

---
+++ Code/CygSoft.SmartSession.Domain.UnitTests/Tests/WeightedProgressCalculatorTests.cs
@@ -0,0 +1,81 @@
+﻿using CygSoft.SmartSession.Domain.Common;
+using CygSoft.SmartSession.Infrastructure;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.Domain.UnitTests.Tests
+{
+    [TestFixture]
+    public class WeightedProgressCalculatorTests
+    {
+        [Test]
+        public void WeightedProgressCalculator_CalculateTotalProgress_Test_1()
+        {
+            var calculator = new WeightedProgressCalculator();
+
+            calculator.Add(new WeightedObj(100) { Weighting = 6000 });
+            calculator.Add(new WeightedObj(60) { Weighting = 12000 });
+            calculator.Add(new WeightedObj(25) { Weighting = 6000 });
+
+            var progress = calculator.CalculateTotalProgress();
+
+            Assert.That(progress, Is.EqualTo(61.25));
+        }
+
+        [Test]
+        public void WeightedProgressCalculator_CalculateTotalProgress_Test_2()
+        {
+            var calculator = new WeightedProgressCalculator();
+
+            calculator.Add(new WeightedObj(100) { Weighting = 25 });
+            calculator.Add(new WeightedObj(60) { Weighting = 40 });
+            calculator.Add(new WeightedObj(40) { Weighting = 60 });
+            calculator.Add(new WeightedObj(25) { Weighting = 75 });
+
+            var progress = calculator.CalculateTotalProgress();
+
+            Assert.That(progress, Is.EqualTo(45.875));
+        }
+
+        [Test]
+        public void WeightedProgressCalculator_CalculateTotalProgress_Test_3()
+        {
+            var calculator = new WeightedProgressCalculator();
+
+            calculator.Add(new WeightedObj(100) { Weighting = 25 });
+            calculator.Add(new WeightedObj(100) { Weighting = 40 });
+            calculator.Add(new WeightedObj(100) { Weighting = 60 });
+            calculator.Add(new WeightedObj(100) { Weighting = 40 });
+            calculator.Add(new WeightedObj(100) { Weighting = 60 });
+            calculator.Add(new WeightedObj(100) { Weighting = 40 });
+            calculator.Add(new WeightedObj(100) { Weighting = 60 });
+            calculator.Add(new WeightedObj(100) { Weighting = 40 });
+            calculator.Add(new WeightedObj(100) { Weighting = 60 });
+            calculator.Add(new WeightedObj(100) { Weighting = 75 });
+
+            var progress = calculator.CalculateTotalProgress();
+
+            Assert.That(progress, Is.EqualTo(100));
+        }
+
+        public class WeightedObj : IWeightedEntity
+        {
+            private readonly double percentCompleted;
+
+            public WeightedObj(double percentComplete)
+            {
+                percentCompleted = percentComplete;
+            }
+            public int Weighting { get; set; }
+
+            public double PercentCompleted()
+            {
+                return percentCompleted;
+            }
+        }
+    }
+}
---
+++ Code/CygSoft.SmartSession.Domain/Common/WeightedProgressCalculator.cs
@@ -0,0 +1,37 @@
+﻿using CygSoft.SmartSession.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CygSoft.SmartSession.Domain.Common
+{
+    public class WeightedProgressCalculator
+    {
+        private List<IWeightedEntity> weightedEntities = new List<IWeightedEntity>();
+
+        public void Add(IWeightedEntity item)
+        {
+            weightedEntities.Add(item);
+        }
+
+        public double CalculateTotalProgress()
+        {
+            if (weightedEntities.Count == 0)
+                return 0;
+
+            var countOfWeightings = weightedEntities.Count;
+            var sumOfWeightings = weightedEntities.Sum(w => w.Weighting);
+
+            return weightedEntities.Select(w => CalculateWeightedValue(w, countOfWeightings, sumOfWeightings)).Sum();
+        }
+
+        private double CalculateWeightedValue(IWeightedEntity item, int countOfItems, int sumOfItemWeightings)
+        {
+            double weightingSlice = (((double)item.Weighting / sumOfItemWeightings) * 100);
+            double val = (item.PercentCompleted() / 100d) * weightingSlice;
+            return val;
+        }
+    }
+}
