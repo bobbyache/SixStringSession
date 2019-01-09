cd501f7a83557395eee4b680c890fd471c6a4e40
Rob Blake <robbie.a.blake@gmail.com>
2019-01-09 12:03:50 +0200

    Basic Manual Tracking popup tooltip weorking. -> still need to push style into resource dictionary.

---
+++ Code/CygSoft.SmartSession.Desktop.Controls/PopupEx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Controls.Primitives;
+
+namespace CygSoft.SmartSession.Desktop.Controls
+{
+    public class PopupEx : Popup
+    {
+        protected override void OnOpened(EventArgs e)
+        {
+            var friend = this.PlacementTarget;
+            friend.QueryCursor += friend_QueryCursor;
+
+            base.OnOpened(e);
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            var friend = this.PlacementTarget;
+            friend.QueryCursor -= friend_QueryCursor;
+
+            base.OnClosed(e);
+        }
+
+        private void friend_QueryCursor(object sender, System.Windows.Input.QueryCursorEventArgs e)
+        {
+            this.HorizontalOffset += +0.1;
+            this.HorizontalOffset += -0.1;
+        }
+    }
+}
