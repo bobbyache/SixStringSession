7544dd957d83b768134a3b31124c2ec221164e89
Rob Blake <robbie.a.blake@gmail.com>
2019-01-10 15:36:42 +0200

    Believe the slider binding problem is finally solved with this prototype.

---
+++ ProtoTypes/CustomSliderCtrl/SliderControl/SliderControl/Slider_A_Window.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SliderControl
+{
+    /// <summary>
+    /// Interaction logic for Slider_A_Window.xaml
+    /// </summary>
+    public partial class Slider_A_Window : Window
+    {
+        public Slider_A_Window()
+        {
+            InitializeComponent();
+            this.Loaded += Slider_A_Window_Loaded;
+        }
+
+        private void Slider_A_Window_Loaded(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
---
+++ ProtoTypes/CustomSliderCtrl/SliderControl/SliderControl/SomeTemplateViewModel.cs
@@ -0,0 +1,34 @@
+﻿using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace SliderControl
+{
+    public class SomeTemplateViewModel : ViewModelBase
+    {
+        private double _value;
+
+        public SomeTemplateViewModel()
+        {
+            // Create command setting Value as Slider's NewValue
+            ValueChangedCommand = new RelayCommand<RoutedPropertyChangedEventArgs<double>>(
+                args => Value = args.NewValue);
+
+            Value = 35;
+        }
+
+        public ICommand ValueChangedCommand { get; set; }
+
+        public double Value
+        {
+            get { return _value; }
+            set { _value = value; RaisePropertyChanged(); } // Notify UI
+        }
+    }
+}
