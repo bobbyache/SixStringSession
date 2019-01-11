98ee934ea496b55add1400f0e0ca0af357422514
Rob Blake <robbie.a.blake@gmail.com>
2019-01-11 09:41:25 +0200

    Added a "calculated property to the slider control prototype.

--- ProtoTypes/CustomSliderCtrl/SliderControl/SliderControl/SomeTemplateViewModel.cs
+++ ProtoTypes/CustomSliderCtrl/SliderControl/SliderControl/SomeTemplateViewModel.cs
@@ -10,25 +10,76 @@ using System.Windows.Input;
 
 namespace SliderControl
 {
+
     public class SomeTemplateViewModel : ViewModelBase
     {
-        private double _value;
-
         public SomeTemplateViewModel()
         {
             // Create command setting Value as Slider's NewValue
             ValueChangedCommand = new RelayCommand<RoutedPropertyChangedEventArgs<double>>(
-                args => Value = args.NewValue);
+                args => CurrentManualProgress = args.NewValue);
 
-            Value = 35;
+            DecreaseManualProgressCommand = new RelayCommand(() => DecreaseManualProgress(), () => true);
+            IncreaseManualProgressCommand = new RelayCommand(() => IncreaseManualProgress(), () => true);
+
+            InitialManualProgress = 35;
+            CurrentManualProgress = 35;
+        }
+
+        private double currentManualProgress;
+        public double CurrentManualProgress
+        {
+            get { return currentManualProgress; }
+            set
+            {
+                currentManualProgress = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged("ProgressInformationText");
+            }
+        }
+
+        private double initialManualProgress;
+        public double InitialManualProgress
+        {
+            get { return initialManualProgress; }
+            set
+            {
+                initialManualProgress = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged("ProgressInformationText");
+            }
+        }
+
+        public string ProgressInformationText
+        {
+            get
+            {
+                bool positive = CurrentManualProgress > InitialManualProgress;
+                double difference = Math.Abs(CurrentManualProgress - InitialManualProgress);
+
+                string positiveSign = positive ? "+" : "-";
+
+                if (difference == 0)
+                    return $"{CurrentManualProgress}%";
+                else
+                    return $"{CurrentManualProgress}% ({positiveSign}{difference})";
+            }
         }
 
         public ICommand ValueChangedCommand { get; set; }
+        public RelayCommand DecreaseManualProgressCommand { get; private set; }
+        public RelayCommand IncreaseManualProgressCommand { get; private set; }
 
-        public double Value
+        private void DecreaseManualProgress()
         {
-            get { return _value; }
-            set { _value = value; RaisePropertyChanged(); } // Notify UI
+            if (CurrentManualProgress > 0)
+                CurrentManualProgress--;
+        }
+
+        private void IncreaseManualProgress()
+        {
+            if (CurrentManualProgress < 100)
+            CurrentManualProgress++;
         }
     }
 }
