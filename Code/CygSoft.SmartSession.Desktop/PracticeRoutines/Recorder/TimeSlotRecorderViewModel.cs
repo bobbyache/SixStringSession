a951fe1edf7c659850a2d284622f62486bf7bff5
Rob Blake <robbie.a.blake@gmail.com>
2019-03-15 09:57:25 +0200

    Some UI tweaking. Added RemainingTimeDisplay. Some Data Triggers for better progress indication.

--- Code/CygSoft.SmartSession.Desktop/PracticeRoutines/Recorder/TimeSlotRecorderViewModel.cs
+++ Code/CygSoft.SmartSession.Desktop/PracticeRoutines/Recorder/TimeSlotRecorderViewModel.cs
@@ -1,13 +1,17 @@
 ﻿using CygSoft.SmartSession.Desktop.Exercises.Recorder;
 using CygSoft.SmartSession.Domain.Recording;
 using CygSoft.SmartSession.Infrastructure;
+using System;
 
 namespace CygSoft.SmartSession.Desktop.PracticeRoutines.Recorder
 {
     public class TimeSlotRecorderViewModel : RecorderViewModel
     {
+        public bool PracticeTimeCompleted { get => (int)TimeSlotExerciseRecorder.RemainingSeconds == 0;  }
         public int AssignedSeconds { get => (int)TimeSlotExerciseRecorder.AssignedSeconds; }
         public int RemainingSeconds { get => (int)TimeSlotExerciseRecorder.RemainingSeconds; }
+
+        public int AssignedTimePercentage { get => (int)Math.Round((TimeSlotExerciseRecorder.RecordedSeconds / TimeSlotExerciseRecorder.AssignedSeconds) * 100, 0); }
 
         public string AssignedTimeDisplay { get => TimeFuncs.DisplayTimeFromSeconds(AssignedSeconds); }
         public string RemainingTimeDisplay { get => TimeFuncs.DisplayTimeFromSeconds(RemainingSeconds); }
@@ -26,6 +30,8 @@ namespace CygSoft.SmartSession.Desktop.PracticeRoutines.Recorder
             RaisePropertyChanged(() => RemainingSeconds);
             RaisePropertyChanged(() => AssignedTimeDisplay);
             RaisePropertyChanged(() => RemainingTimeDisplay);
+            RaisePropertyChanged(() => AssignedTimePercentage);
+            RaisePropertyChanged(() => PracticeTimeCompleted);
         }
     }
 }
