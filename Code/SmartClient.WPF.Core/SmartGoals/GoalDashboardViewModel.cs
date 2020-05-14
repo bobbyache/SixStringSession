93e04b533908c2db1973c18e0de6a2e9bb13fef9
bobbyache <robbie.a.blake@gmail.com>
2020-05-14 10:59:23 +0200

    Added some styling to the goal dashboard

--- Code/SmartClient.WPF.Core/SmartGoals/GoalDashboardViewModel.cs
+++ Code/SmartClient.WPF.Core/SmartGoals/GoalDashboardViewModel.cs
@@ -10,18 +10,25 @@ namespace SmartGoals
     public class GoalDashboardViewModel: Screen
     {
         private readonly GoalManager goalManager;
+        private IGoalDetail goal;
 
-        public IList<IGoalTaskSummary> TaskSummaries { get { return this.goalManager.GetTaskSummaries(); } }
+        public IList<IGoalTaskSummary> TaskSummaries { get { return this.goal.TaskSummaries; } }
         private IEventAggregator eventAggregator { get; }
+
+        public string Title { get { return this.goal.Title; } }
+        public int PercentProgress {  get { return this.goal.PercentProgress;  } }
 
         public GoalDashboardViewModel(IEventAggregator eventAggregator, GoalManager goalManager)
         {
             // goalManager.Save(@"C:\Code\dummy_goal_saved.json");
-
+            
+            
             this.eventAggregator = eventAggregator;
             this.goalManager = goalManager;
             this.eventAggregator.SubscribeOnUIThread(this);
-            goalManager.Open(@"C:\Code\dummy_goal.json");
+            goalManager.Open(@"C:\Code\dummy_goal_2.json");
+            this.goal = this.goalManager.GetDetail();
+            
         }
     }
 }
