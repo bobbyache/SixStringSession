6077520e1ba26c6c7bbf10d468ac7e2e43840517
bobbyache <robbie.a.blake@gmail.com>
2020-06-01 14:53:39 +0200

    Clean up settings

--- Code/SmartClient.WPF.Core/SmartGoals/Services/SettingsService.cs
+++ Code/SmartClient.WPF.Core/SmartGoals/Services/SettingsService.cs
@@ -4,17 +4,19 @@ namespace SmartGoals.Services
 {
     public class SettingsService : ISettingsService
     {
+        private const string INITIAL_PROJECT_DIRECTORY = "InitialProjectDirectory";
+
         public virtual string InitialProjectDirectory
         {
             get
             {
-                if (ConfigurationManager.AppSettings["InitialProjectDirectory"] != null)
-                    return ConfigurationManager.AppSettings["InitialProjectDirectory"];
+                if (ConfigurationManager.AppSettings[INITIAL_PROJECT_DIRECTORY] != null)
+                    return ConfigurationManager.AppSettings[INITIAL_PROJECT_DIRECTORY];
                 return "";
             }
             set
             {
-                ConfigurationManager.AppSettings["InitialProjectDirectory"] = value;
+                ConfigurationManager.AppSettings[INITIAL_PROJECT_DIRECTORY] = value;
             }
         }
     }
